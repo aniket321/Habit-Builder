73f11bd7245fc3d0fb1bcf47ecb1f28d4e589d24
aniket.shashikant <aniket.shashikant@hashedin.com>
2020-07-15 14:54:53 +0530

    updation in package.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 
 const app = express();
@@ -26,6 +27,13 @@ mongoose
 // Routes file
 app.use('/api/users', userRouter);
 app.use('/api/posts', postRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
